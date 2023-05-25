be509cab6c195eec74c92bc728b196424350156f
Dylan Garcia <50163127+nzx0@users.noreply.github.com>
2023-05-24 23:54:05 -0500

    probably breaking everything just to make it executable from console

--- src/me.EncryptedShoesHD.pixel8or/commands/PixelatorCommand.java
+++ src/me.EncryptedShoesHD.pixel8or/commands/PixelatorCommand.java
@@ -1,7 +1,9 @@
 package me.EncryptedShoesHD.pixel8or.commands;
 
 import me.EncryptedShoesHD.pixel8or.Pixel8or;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,15 +19,30 @@ public class PixelatorCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
-        if(cs instanceof Player) {
+        // get the parameters from the command
+        String fileName = args[0];
+        String worldName = args[1]; // this is the new argument for the world name, kinda useless
+        String x = args[2];
+        String y = args[3];
+        String z = args[4];
+
+        // get the world. if it's a player, use the player's world. if it's the console, use the specified world name.
+        World world;
+        if (cs instanceof Player) {
             Player p = (Player) cs;
-            String fileName = args[0];
-            String x = args[1];
-            String y = args[2];
-            String z = args[3];
-            Location location = new Location(p.getLocation().getWorld(), Double.parseDouble(x), Double.parseDouble(y), Double.parseDouble(z));
-            plugin.getArtist().draw(location, fileName + ".png");
+            world = p.getWorld();
+        } else {
+            world = Bukkit.getWorld(worldName);
+            if (world == null) {
+                cs.sendMessage("The world " + worldName + " does not exist.");
+                return true;
+            }
         }
-        return false;
+
+        // create the location and call artist
+        Location location = new Location(world, Double.parseDouble(x), Double.parseDouble(y), Double.parseDouble(z));
+        plugin.getArtist().draw(location, fileName + ".png");
+
+        return true;
     }
 }
